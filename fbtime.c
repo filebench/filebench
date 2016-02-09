77fa3a2466d367480601b9984196ba231e707a13
Vasily Tarasov <tarasov@vasily.name>
2016-02-09 18:14:07 -0500

    Cleaning up fbtime.c. Mostly code related to rdtsc.

--- fbtime.c
+++ fbtime.c
@@ -1,14 +1,9 @@
-#include "fbtime.h"
-#include "config.h"
-
 #include <sys/time.h>
 #include <stdlib.h>
-
-/*
- * Routines to access high resolution system time, initialize and
- * shutdown filebench, log filebench run progress and errors, and
- * access system information strings.
- */
+#include <stdio.h>
+#include "fbtime.h"
+#include "config.h"
+#include "filebench.h"
 
 /*
  * If we dont have gethrtime() function provided by the environment
@@ -18,28 +13,36 @@
 
 #ifndef HAVE_GETHRTIME
 #ifdef USE_RDTSC
+
 /*
- * Lets us use the rdtsc instruction to get highres time.
- * Thanks to libmicro
+ * Multiplier to convert CPU cycles returned by rdtsc() to nanoseconds.  In
+ * other words nanoseconds per CPU cycle. E.g.,  for a 1GHz CPU it is equal to
+ * 1.0.  For currently available systems the factor varies between 10.0
+ * (100MHz) to 0.2 (5GHz).
+ *
+ * Notice, we do not take into account CPU frequency scaling. Therefore,
+ * results might be incorrect for sytems that use CPU frequency scaling.
+ *
  */
-uint64_t	cpu_hz = 0;
+static double rdtsc2nan_mul = 0;
 
 /*
  * Uses the rdtsc instruction to get high resolution (cpu
- * clock ticks) time. Only used for non Sun compiles.
+ * clock ticks) time. Only used for explicitly requested
+ * non-Solaris compiles.
  */
-__inline__  unsigned long long
+__inline__  uint64_t
 rdtsc(void)
 {
-	unsigned long long x;
+	unsigned long long int x;
 	__asm__ volatile(".byte 0x0f, 0x31" : "=A" (x));
-	return (x);
+	return x;
 }
 
 /*
- * Get high resolution time in nanoseconds. This is the version
- * used when not compiled for Sun systems. It uses rdtsc call to
- * get clock ticks and converts to nanoseconds
+ * Get high resolution time in nanoseconds. This is the version used when
+ * explicitly requested on non-Solaris systems. It uses rdtsc call to get
+ * clock ticks and converts to nanoseconds.
  */
 hrtime_t
 gethrtime(void)
@@ -47,14 +50,15 @@ gethrtime(void)
 	hrtime_t hrt;
 
 	/* convert to nanosecs and return */
-	hrt = ((rdtsc() * ((double)1000000000UL / cpu_hz)));
-	return (hrt);
+	hrt = rdtsc() * rdtsc2nan_mul;
+
+	return hrt;
 }
 
 /*
  * Gets CPU clock frequency in MHz from cpuinfo file.
  * Converts to cpu_hz and stores in cpu_hz global uint64_t.
- * Only used for non Sun compiles.
+ * Only used for non-Solaris compiles.
  */
 static uint64_t
 parse_cpu_hz(void)
@@ -67,38 +71,42 @@ parse_cpu_hz(void)
 	double hertz = -1;
 	uint64_t hz;
 
-	if ((cpuinfo = fopen("/proc/cpuinfo", "r")) == NULL) {
+	cpuinfo = fopen("/proc/cpuinfo", "r");
+	if (!cpuinfo) {
 		filebench_log(LOG_ERROR, "open /proc/cpuinfo failed: %s",
 		    strerror(errno));
 		filebench_shutdown(1);
 	}
+
 	while (!feof(cpuinfo)) {
 		char buffer[80];
 
 		fgets(buffer, 80, cpuinfo);
-		if (strlen(buffer) == 0) continue;
-		if (strncasecmp(buffer, "cpu MHz", 7) == 0) {
+		if (!strlen(buffer))
+			continue;
+		if (!strncasecmp(buffer, "cpu MHz", 7)) {
 			char *token = strtok(buffer, ":");
 
-			if (token != NULL) {
+			if (token) {
 				token = strtok((char *)NULL, ":");
 				hertz = strtod(token, NULL);
 			}
 			break;
 		}
 	}
+
 	hz = hertz * 1000000;
 
 	fclose(cpuinfo);
 
-	return (hz);
+	return hz;
 }
 #else /* USE_RDTSC */
 
 /*
- * Get high resolution time in nanoseconds. This is the version
- * used if compiled for Sun systems. It calls gettimeofday
- * to get current time and converts it to nanoseconds.
+ * Get high resolution time in nanoseconds. This is the version used if
+ * compiled for non-Solaris systems. It calls gettimeofday to get current time
+ * and converts it to nanoseconds.
  */
 hrtime_t
 gethrtime(void)
@@ -110,26 +118,29 @@ gethrtime(void)
 
 	hrt = (hrtime_t)tv.tv_sec * 1000000000UL +
 	    (hrtime_t)tv.tv_usec * 1000UL;
-	return (hrt);
+
+	return hrt;
 }
 #endif /* USE_RDTSC */
 #endif /* HAVE_GETHRTIME */
 
 /*
- * Sets the cpu clock frequency variable or shuts down the
- * run if one is not found.
+ * In case if rdtsc is used, sets the cpu clock frequency variable or shuts
+ * down the run if one is not found.
  */
 void
 clock_init(void)
 {
 #if !defined(HAVE_GETHRTIME) && defined(USE_RDTSC)
+	uint64_t cpu_hz;
+
 	cpu_hz = parse_cpu_hz();
 	if (cpu_hz <= 0) {
 		filebench_log(LOG_ERROR, "Error getting CPU Mhz: %s",
 		    strerror(errno));
 		filebench_shutdown(1);
 	}
+
+	rdtsc2nan_mul = 1000000000.0 / cpu_hz;
 #endif
 }
-
-
